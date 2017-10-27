37de522a2a17c5b0f5615a166a63351aeed236fe
vallefor <madsorcerer@gmail.com>
2017-10-27 11:34:14 +0300

    Read get variable instead of post Add option to fully disable cache

--- server.js
+++ server.js
@@ -8,8 +8,11 @@ var md5 = require('md5');
 var phantomCache = './cache';
 var tmpDirPath = './tmp';
 
+var sendDataWileCacheRefresh=false;
+
 var cacheTime=1000*60*60; //millisec
-cacheTime=1000*5;
+//cacheTime=1000*5;
+cacheTime=false; //disable cache by default
 
 if (!fs.existsSync(tmpDirPath)){
     fs.mkdirSync(tmpDirPath);
@@ -21,91 +24,172 @@ if (!fs.existsSync(phantomCache)){
 
 var working={};
 
+
 let port=9999;
 console.log("Starting... ");
 
-function getPage(url,callback) {
-  var sitepage = null;
-  var phInstance = null;
+function normalizeUrl(url) {
+  url=url.toLowerCase();
+  url=url.replace(/\/$/, "");
+  return url;
+}
 
-  phantom.create([
-//    '--disk-cache=true',
+let pages=[];
+let loadedPages=[];
+let loadedPagesBusy=[];
+let globalPhantom;
+let startTimerGlobal=new Date().getTime();
+phantom.create([
+    '--disk-cache=true',
     '--load-images=false',
     `--disk-cache-path=${phantomCache}`,
-    '--max-disk-cache-size=102400',
+    '--max-disk-cache-size=10240000',
     `--cookies-file=${__dirname}/cache/cookie.txt`,
     //'--debug=true'
   ]).then(ph=> {
-      console.log("created");
-      ph.cookiesEnabled=false;
-      phInstance=ph;
-      return ph.createPage();
-    })
-    .then(page=> {
-      console.log("page here",typeof page);
+    console.log("created");
+    ph.cookiesEnabled=false;
+    globalPhantom=ph;
+    for(let i=0;i<10;i++) {
+      pages.push(ph.createPage());
+      loadedPagesBusy.push(false);
+    }
+    return Promise.all(pages);
+  }).then(pages=>{
+    loadedPages=pages;
+    //console.log("pages",pages);
+    /*console.log("manypage loaded",new Date().getTime()-startTimerGlobal);
+    pageLast.open("https://stockrow.com").then(function(){
+      console.log("on page!");
+    });*/
+  });
 
+function getBrowserTab()
+{
+  for(let i in loadedPages) {
+    if(!loadedPagesBusy[i]) {
+      return { index:i, page:loadedPages[i] };
+    }
+  }
+  return getBrowserTab();
+}
+
+function getPage(url,callback) {
+  let startTimer=new Date().getTime();
+
+  var sitepage = null;
+  var phInstance = null;
+      let pageOb=getBrowserTab();
+      loadedPagesBusy[pageOb.index]=true;
+      let page=pageOb.page;
+
+      console.log("page here",new Date().getTime()-startTimer);
       page.setting('loadImages', false);
       //page.setting('loadCSS', false);
       var resCount=0;
+      //let httpStatus=0;
+      //let exchanger={ httpStatus:0, url:url };
+      let exchanger=globalPhantom.createOutObject();
+      exchanger.url=url;
+      exchanger.normalizeUrl=normalizeUrl;
+      exchanger.requestCounter=0;
+      exchanger.requestSumm=0;
+      page.property('onResourceReceived', function(resource,exchanger){
+        if(resource.stage=="start") {
+          exchanger.requestCounter++;
+          console.log("counter inc",exchanger.requestCounter,exchanger.requestSumm,resource.url);
+        }
+        if(resource.stage=="end") {
+          //console.log('received', resource.url);
+          if(resource.url) {
+            exchanger.requestCounter--;
+            console.log("counter dec", exchanger.requestCounter,exchanger.requestSumm, resource.status, resource.url);
+          }
+          if(exchanger.url==exchanger.normalizeUrl(resource.url)) {
+            exchanger.httpStatus=resource.status;
+            //console.log('onResourceReceived', resource.url, resource.status, exchanger.httpStatus, exchanger.url);
+            //return exchanger;
+          }
+        }
+      },exchanger);
 
-      /*
-      page.property('onResourceReceived', true, function(requestData) {
-        resCount--;
-        console.log("res count onResourceReceived",resCount);
-      });
-      */
+
       page.property('onResourceRequested', function(requestData, request) {
         var arr=requestData.url.split(".");
-        console.log("request",requestData.url);
+        console.log("---------- request",requestData.url);
+        if(
+          requestData.url.indexOf("googlesyndication")>-1
+          ||
+          requestData.url.indexOf("googleads")>-1
+          ||
+          requestData.url.indexOf("gstatic.com/recaptcha")>-1
+          ||
+          requestData.url.indexOf("google-analytics")>-1
+          /*||
+          requestData.url.indexOf("cdn.ravenjs.com")>-1*/
+          ||
+          requestData.url.indexOf("www.google.com/recaptcha")>-1
+        ){
+          request.abort();
+        }
         if(arr[arr.length-1]=="css") {
           request.abort();
         } else {
-          console.log("res count",resCount);
+          //exchanger.requestSumm++;
         }
 
       });
 
       page.open(url).then(function(){
         console.log("on page!");
-        page.property('content').then(function (content) {
-          var returnFunction=function(){
-            callback(content);
-            phInstance.exit();
-          };
-          returnFunction();
-          /*
-          if(resCount==0) {
-            console.log("res count",resCount);
-            returnFunction();
-          } else {
-            var interval=setInterval(function(){
-              console.log("res count",resCount);
-              if(resCount==0) {
-                returnFunction();
+
+        var onPageExec=function() {
+          exchanger.property("httpStatus").then((httpStatus)=> {
+            console.log("here status is",httpStatus,exchanger.httpStatus);
+          });
+
+          page.property('content').then(function (content) {
+
+
+
+            page.evaluate(function () {
+              if (document.getElementById('httpStatus')) {
+                return parseInt(document.getElementById('httpStatus').innerHTML);
+              } else {
+                return null;
               }
-            },10);
+            }).then(function (jsHttpStatus) {
+              let returnFunction = function () {
+                exchanger.property("httpStatus").then((httpStatus)=> {
+                  const status=jsHttpStatus?jsHttpStatus:httpStatus;
+                  console.log("http status is", status);
+                  setTimeout(function(){
+                    callback(JSON.stringify({httpStatus: status, content: content}));
+                  },4000);
 
-            setTimeout(function(){
-              clearInterval(interval);
+                  loadedPagesBusy[pageOb.index]=false;
+                  //phInstance.exit();
+                });
+              };
               returnFunction();
-            },5000);
-          }
-          */
 
-        });
+            });
+
+          });
+        };
+        setTimeout(()=>{
+          onPageExec();
+        },3000);
+
+
       });
-
-    })
-    .catch(error => {
-      console.log(error);
-      phInstance.exit();
-    });
-
 }
 
 
     var server = http.createServer(function(request, response) {
       console.log(request.url);
+      var getVars=request.url.split('?')[1];
+      getVars=qs.parse(getVars);
 
       var body = [];
       request.on('data', function(chunk) {
@@ -113,19 +197,26 @@ function getPage(url,callback) {
       }).on('end', function() {
         body = Buffer.concat(body).toString();
 
-        var post = qs.parse(body);
-        console.log(post);
-        var cacheFile=`${tmpDirPath}/${md5(request.url)}`;
+
+
+        //var post = qs.parse(body);
+        getVars.url=normalizeUrl(getVars.url);
+        console.log(getVars);
+
+        var cacheFile=`${tmpDirPath}/${md5(getVars.url)}`;
 
         var readPage=function() {
-          getPage(post.url,function(content) {
+          getPage(getVars.url,function(content) {
 
             /*if(!working[cacheFile]) {
               working[cacheFile]=true;*/
-              fs.writeFile(cacheFile, content,function(){
-                console.log('delete '+cacheFile);
+            if(cacheTime!==false) {
+              console.log('write cache ' + cacheFile);
+              fs.writeFile(cacheFile, content, function () {
+                console.log('delete ' + cacheFile);
                 delete working[cacheFile];
               });
+            }
             //}
 
             response.end(content);
@@ -135,58 +226,67 @@ function getPage(url,callback) {
           });
         };
 
+        if(cacheTime!==false) {
+          fs.stat(cacheFile, (err, stat)=> {
 
-        fs.stat(cacheFile, (err,stat)=>{
+            var readAndSend = function () {
+              fs.readFile(cacheFile, (err, data)=> {
+                if (data) {
+                  response.end(data);
+                } else {
+                  readPage();
+                }
+              });
+            };
 
-          var readAndSend=function() {
-            fs.readFile(cacheFile, (err, data)=> {
-              if (data) {
-                response.end(data);
+            if (stat) {
+              //console.log("stat",stat,err,cacheFile);
+
+              var curDate = new Date();
+              var fileDate = new Date(stat.birthtime);
+              var timeDif = curDate.getTime() - fileDate.getTime();
+
+              if (timeDif > cacheTime) {
+                if (!working[cacheFile]) {
+                  working[cacheFile] = true;
+                  getPage(getVars.url, function (content) {
+                    if (!sendDataWileCacheRefresh) {
+                      response.end(content);
+                    }
+                    fs.writeFile(cacheFile, content, function () {
+                      delete working[cacheFile];
+                    });
+                  });
+                }
+                if (sendDataWileCacheRefresh) {
+                  readAndSend();
+                }
               } else {
+                readAndSend();
+              }
+
+            } else {
+              if (working[cacheFile]) {
+
+                var intervalWork = setInterval(()=> {
+                  if (!working[cacheFile]) {
+                    clearInterval(intervalWork);
+                    readAndSend();
+                  } else {
+                    console.log("working = true");
+                  }
+                }, 100);
+
+              } else {
+                working[cacheFile] = true;
                 readPage();
               }
-            });
-          };
-
-          if(stat) {
-            console.log("stat",stat,err,cacheFile);
-
-            var curDate=new Date();
-            var fileDate=new Date(stat.birthtime);
-            var timeDif=curDate.getTime()-fileDate.getTime();
-
-            if(timeDif>cacheTime) { //10 min
-              if(!working[cacheFile]) {
-                working[cacheFile]=true;
-                getPage(post.url,function(content) {
-                  fs.writeFile(cacheFile, content,function(){
-                    delete working[cacheFile];
-                  });
-                });
-              }
-              readAndSend();
-            } else {
-              readAndSend();
             }
-
-          } else {
-            if(working[cacheFile]) {
-
-              var intervalWork=setInterval(()=>{
-                if(!working[cacheFile]) {
-                  clearInterval(intervalWork);
-                  readAndSend();
-                } else {
-                  console.log("working = true");
-                }
-              },100);
-
-            } else {
-              working[cacheFile]=true;
-              readPage();
-            }
-          }
-        });
+          });
+        } else {
+          console.log('cache disabled');
+          readPage();
+        }
 
 
       });
