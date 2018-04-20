62f0e0e5d8522ad950cd2d74fb6905873cf65374
vallefor <madsorcerer@gmail.com>
2018-04-20 11:34:11 +0300

    posibility to do screenshots

--- server.js
+++ server.js
@@ -15,6 +15,14 @@ const closeChromeTimeout=1000*60*5; //close chrome if no requests for 5 minutes
 let browser;
 let pagesRender=0;
 
+process.stdin.resume();
+process.on('exit', ()=>{
+  if(browser) {
+    browser.close();
+  }
+  process.exit();
+});
+
 if (enableCache && !fs.existsSync(cacheDir)) {
     fs.mkdirSync(cacheDir);
 }
@@ -47,68 +55,103 @@ async function getPage(url, options={}) {
     browser = await puppeteer.launch({args: ['--no-sandbox']});
   }
   const page = await browser.newPage();
-  await page.setRequestInterception(true);
 
   //do not load images, svg and css
-  page.on('request', interceptedRequest => {
-    if (
-      interceptedRequest.url.endsWith('.png') ||
-      interceptedRequest.url.endsWith('.jpg') ||
-      interceptedRequest.url.endsWith('.jpeg') ||
-      interceptedRequest.url.endsWith('.gif') ||
-      interceptedRequest.url.endsWith('.svg') ||
-      interceptedRequest.url.endsWith('.css')
-    ) {
-      interceptedRequest.abort();
-    } else {
-      interceptedRequest.continue();
-    }
-  });
-
-  await page.setViewport({width: 1280, height: 800 });
-  const response=await page.goto(url);
-  try {
-    await page.waitForFunction(function () {
-      if (document.querySelectorAll('.loadingText').length == 0) {
-        return true;
+  if(options.as!=='png') {
+    await page.setRequestInterception(true);
+    page.on('request', interceptedRequest => {
+      if (
+        interceptedRequest.url.endsWith('.png') ||
+        interceptedRequest.url.endsWith('.jpg') ||
+        interceptedRequest.url.endsWith('.jpeg') ||
+        interceptedRequest.url.endsWith('.gif') ||
+        interceptedRequest.url.endsWith('.svg') ||
+        interceptedRequest.url.endsWith('.css')
+      ) {
+        interceptedRequest.abort();
       } else {
-        const arr = document.querySelectorAll('.loadingText');
-        let num = arr.length;
-        for (var i in arr) {
-          let cur = arr[i];
-          while (cur) {
-            if (cur.style && cur.style.display == 'none') {
-              num--;
-              cur=false;
-              break;
-            }
-            if(cur) {
-              cur = cur.parentNode;
+        interceptedRequest.continue();
+      }
+    });
+  } else {
+    console.log('As png. Load full page.');
+  }
+
+  console.log('Setting Viewport');
+  await page.setViewport({width: options.width?options.width:1280, height: options.height?options.height:800 });
+  //await page.waitForNavigation({ waitUntil: 'networkidle0' });
+  console.log('Goto...');
+  const response=await page.goto(url,options.goto?options.goto:{});
+  console.log('Goto done');
+  if(options.square) {
+    let square=await page.evaluate(()=>{
+      const height=document.body.clientHeight;
+      const width=document.body.clientWidth;
+      if(width>height) {
+        return height;
+      }
+      if(height>=width) {
+        return width;
+      }
+    });
+    console.log(`square = ${square}`);
+    await page.setViewport({width: square, height: square });
+
+  }
+  if(!options.goto) {
+    try {
+      await page.waitForFunction(function () {
+        if (document.querySelectorAll('.loadingText').length == 0) {
+          return true;
+        } else {
+          const arr = document.querySelectorAll('.loadingText');
+          let num = arr.length;
+          for (var i in arr) {
+            let cur = arr[i];
+            while (cur) {
+              if (cur.style && cur.style.display == 'none') {
+                num--;
+                cur = false;
+                break;
+              }
+              if (cur) {
+                cur = cur.parentNode;
+              }
             }
           }
-        }
 
-        if (num === 0) {
-          return true;
+          if (num === 0) {
+            return true;
+          }
+          //document.querySelectorAll('.loadingText')[0].parentNode.parentNode.parentNode.style.display
         }
-        //document.querySelectorAll('.loadingText')[0].parentNode.parentNode.parentNode.style.display
-      }
-    }, {timeout: 2000});
-  } catch(error) {
-    console.log('error',error);
+      }, {timeout: 2000});
+    } catch (error) {
+      console.log('error', error);
+    }
   }
+
   if(url.indexOf('/interactive_chart/')>-1) {
     await page.waitFor(500);
   }
-  let iReturn=await page.content();
-  page.close();
-  pagesRender++;
+  if(options.as==='png') {
+    console.log('screenshot start!');
+    let iReturn = await page.screenshot({ omitBackground:true, type:'png' });
+    console.log('screenshot done!');
+    page.close();
+    pagesRender++;
+    return {page: iReturn, headers: response.headers};
+  } else {
+    let iReturn = await page.content();
+    page.close();
+    pagesRender++;
 
-  if(options.removeScripts) {
-    iReturn=iReturn.replace(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi,'');
+    if (options.removeScripts) {
+      iReturn = iReturn.replace(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi, '');
+    }
+
+    return {page: iReturn, headers: response.headers};
   }
-
-  return {page:iReturn,headers:response.headers};
 }
 
 
@@ -163,11 +206,20 @@ app.get('/', function (req, res) {
       console.log(`from cache: ${req.query.url}`);
       res.send(cache.page);
     } else {
-      console.log(`request page: ${req.query.url}`);
-      getPage(req.query.url, { removeScripts:true }).then((data)=> {
-        Cacher.setCache(req.query.url,data);
-        res.send(data.page);
-      });
+      console.log(`request page: ${req.query.url} as=${req.query.as}`, req.query);
+      if(req.query.as=='png') {
+        console.log('var 1');
+        getPage(req.query.url, { goto:{ waitUntil:'networkidle0' }, as:'png', square:req.query.square?true:false }).then((data) => {
+          //Cacher.setCache(req.query.url, data);
+          res.send(data.page);
+        });
+      } else {
+        console.log('var 2');
+        getPage(req.query.url, {removeScripts: true}).then((data) => {
+          Cacher.setCache(req.query.url, data);
+          res.send(data.page);
+        });
+      }
     }
   } else {
     res.send(new Date());
